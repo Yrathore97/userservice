0a87047f932ee43feb4653593360088eff327912
apple <nikhiljain1203@gmail.com>
2023-11-18 22:50:05 +0530

    OAUTH

--- src/main/java/dev/nikhil/userservicetestfinal/services/AuthService.java
+++ src/main/java/dev/nikhil/userservicetestfinal/services/AuthService.java
@@ -6,42 +6,74 @@ import dev.nikhil.userservicetestfinal.models.SessionStatus;
 import dev.nikhil.userservicetestfinal.models.User;
 import dev.nikhil.userservicetestfinal.repositories.SessionRepository;
 import dev.nikhil.userservicetestfinal.repositories.UserRepository;
-import org.apache.commons.lang3.RandomStringUtils;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.MacAlgorithm;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.util.MultiValueMapAdapter;
 
+import javax.crypto.SecretKey;
+import java.util.Date;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 @Service
 public class AuthService {
     private UserRepository userRepository;
     private SessionRepository sessionRepository;
-    private BCryptPasswordEncoder bCryptPasswordEncoder;
+    //private BCryptPasswordEncoder bCryptPasswordEncoder;
 
-    public AuthService(UserRepository userRepository, SessionRepository sessionRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
+    public AuthService(UserRepository userRepository, SessionRepository sessionRepository) {
         this.userRepository = userRepository;
         this.sessionRepository = sessionRepository;
-        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+        //this.bCryptPasswordEncoder = bCryptPasswordEncoder;
     }
 
     public ResponseEntity<UserDto> login(String email, String password) {
         Optional<User> userOptional = userRepository.findByEmail(email);
 
+        //User does not exist
         if (userOptional.isEmpty()) {
             return null;
         }
 
         User user = userOptional.get();
-        if (!bCryptPasswordEncoder.matches(password, user.getPassword())) {
-            return null;
-        }
 
-        String token = RandomStringUtils.randomAlphanumeric(30);
+        //Validation
+//        if (!bCryptPasswordEncoder.matches(password, user.getPassword())) {
+//            return null;
+//        }
+
+        //String token = RandomStringUtils.randomAlphanumeric(30);
+
+//        String message = "{\n" +
+//        "   \"email\": \"naman@scaler.com\",\n" +
+//        "   \"roles\": [\n" +
+//        "      \"mentor\",\n" +
+//        "      \"ta\"\n" +
+//        "   ],\n" +
+//        "   \"expirationDate\": \"23rdOctober2023\"\n" +
+//        "}";
+//        // user_id
+//        // user_email
+//        // roles
+//        byte[] content = message.getBytes(StandardCharsets.UTF_8);
+
+        Map<String, Object> jsonForJwt = new HashMap<>();
+        jsonForJwt.put("email", user.getEmail());
+        jsonForJwt.put("roles", user.getRoles());
+        jsonForJwt.put("expirationDate", new Date());
+        //if(xx =!null)
+        jsonForJwt.put("createdAt" , new Date());
+
+        MacAlgorithm alg = Jwts.SIG.HS256;
+        SecretKey key = alg.key().build();
+
+        String token = Jwts.builder().claims(jsonForJwt).signWith(key, alg).compact();
 
         Session session = new Session();
         session.setSessionStatus(SessionStatus.ACTIVE);
@@ -49,15 +81,13 @@ public class AuthService {
         session.setUser(user);
         sessionRepository.save(session);
 
-        UserDto userDto = new UserDto();
+        UserDto userDto = User.from(user);
 
 //        Map<String, String> headers = new HashMap<>();
 //        headers.put(HttpHeaders.SET_COOKIE, token);
 
         MultiValueMapAdapter<String, String> headers = new MultiValueMapAdapter<>(new HashMap<>());
         headers.add(HttpHeaders.SET_COOKIE, "auth-token:" + token);
-
-
 
         ResponseEntity<UserDto> response = new ResponseEntity<>(userDto, headers, HttpStatus.OK);
 //        response.getHeaders().add(HttpHeaders.SET_COOKIE, token);
@@ -84,7 +114,7 @@ public class AuthService {
     public UserDto signUp(String email, String password) {
         User user = new User();
         user.setEmail(email);
-        user.setPassword(bCryptPasswordEncoder.encode(password));
+        //user.setPassword(bCryptPasswordEncoder.encode(password));
 
         User savedUser = userRepository.save(user);
 
@@ -97,6 +127,16 @@ public class AuthService {
         if (sessionOptional.isEmpty()) {
             return null;
         }
+        MacAlgorithm alg = Jwts.SIG.HS256;
+        SecretKey key = alg.key().build();
+
+        Claims claims =
+                Jwts.parser().verifyWith(key).build().parseSignedClaims(token).getPayload();
+
+//        if(exiprytime > currentdate) {
+//
+//        }
+        // check login device
 
         return SessionStatus.ACTIVE;
     }
